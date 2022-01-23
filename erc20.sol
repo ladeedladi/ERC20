9d787cbbc317f1fe2bcab0ab4b56acd1afdc714b
ladeed <muhammed.ladeed123@gmail.com>
2022-01-23 12:12:47 +0530

    created  ico for the token

--- erc20.sol
+++ erc20.sol
@@ -1,4 +1,3 @@
-
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.7.0 <0.9.0;
@@ -36,7 +35,7 @@ function balanceOf(address tokenOwner)  public view override returns (uint balan
     return balances[tokenOwner];
 }
 
-function transfer(address to, uint tokens)  public override returns (bool success){
+function transfer(address to, uint tokens)  public override virtual returns (bool success){
     require(balances[msg.sender]>=tokens);
     balances[to]+=tokens;
     balances[msg.sender]-=tokens;
@@ -58,7 +57,7 @@ return allowed[tokenOwner][spender];
 }
 
 
-function transferFrom(address from, address to, uint tokens) public override returns (bool success){
+function transferFrom(address from, address to, uint tokens) public override virtual returns (bool success){
     require(allowed[from][to]>=tokens);
     require(balances[from]>=tokens);
     balances[to]+=tokens;
@@ -66,5 +65,90 @@ function transferFrom(address from, address to, uint tokens) public override ret
     return true;
 }
 
+
+}
+
+contract ICO is Block{
+    address public manager;
+    address payable public deposit;
+    uint public tokenPrice= 0.1 ether;
+    uint public cap= 500 ether;
+    uint public raisedAmount;
+    uint public icoStart=block.timestamp;
+    uint public icoEnd=block.timestamp+3;
+    uint public tokenTradeTime=icoEnd+3600;
+    uint public maxInvest=10 ether;
+    uint public minInvest=0.1 ether;
+    enum State{beforeStart,afterEnd,running,halted}
+    State public icoState;
+
+    event Invest(address investor,uint value,uint token);
+    constructor(address payable _deposit){
+    manager=msg.sender;
+    deposit=_deposit;
+    icoState=State.beforeStart;
+    }
+    modifier onlyManager(){
+    require(msg.sender==manager);
+    _;
+    }
+
+    function halt() public onlyManager{
+        icoState=State.halted;
+    }
+    function resume() public onlyManager{
+        icoState=State.running;
+    }
+    function changeDepositAddress(address payable newDeposit) public onlyManager{
+        deposit =newDeposit;
+    }
+
+    function getState() public view returns(State){
+        if(icoState==State.halted){
+            return State.halted;
+        }else if(block.timestamp<icoStart){
+            return State.beforeStart;
+        }else if(block.timestamp>=icoStart && block.timestamp<=icoEnd){
+            return State.running;
+        }else{
+            return State.afterEnd;
+        }
+    }
+
+    function invest() payable public returns(bool){
+        icoState=getState();
+        require(icoState==State.running);
+        require(msg.value>=minInvest && msg.value<=maxInvest);
+        raisedAmount+=msg.value;
+        require(raisedAmount<=cap);
+        uint tokens=msg.value/tokenPrice;
+        balances[msg.sender]+=tokens;
+        balances[founder]-=tokens;
+        deposit.transfer(msg.value);
+        emit Invest(msg.sender,msg.value,tokens);
+        return true;
+    }
+    function burn() public onlyManager returns(bool){
+        icoState=getState();
+        require(icoState==State.afterEnd);
+        balances[founder]=0;
+        return true;
+    }
+
+    function transfer(address to, uint tokens)  public override returns(bool success){
+        require(block.timestamp>tokenTradeTime);
+        super.transfer(to,tokens);
+        return true;
+    }
+    function transferFrom(address from, address to, uint tokens) public override  returns (bool success){
+        require(block.timestamp>tokenTradeTime);
+        super.transferFrom(from,to,tokens);
+        return true;
+    }
+    
+    receive() external payable{
+        invest();
+    }
+    
 
 }
